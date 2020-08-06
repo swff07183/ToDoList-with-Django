69765fb1a57697fefeeab8b33ddb36b2d478c711
swff07183 <swff07183@gmail.com>
2020-08-06 14:06:18 +0900

    0806

---
+++ my_to_do_app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
+
+# Create your views here.
+def index(request) :
+    todos = Todo.objects.all()
+    content = {'todos' : todos}
+    return render(request, 'my_to_do_app/index.html', content)
+
+def createTodo(request) :
+    user_input_str = request.POST['todoContent']
+    new_todo = Todo(content = user_input_str)
+    new_todo.save()
+
+    return HttpResponseRedirect(reverse('index'))
+    #return HttpResponse("create Todo를 할 거야! =>" + user_input_str)
+
+
+def doneTodo(request) :
+    done_todo_id = request.GET['todoNum']
+    print("doneTodo : 완료한 todo의 id", done_todo_id)
+    return HttpResponseRedirect(reverse('index'))
+
+
+
+def deleteTodo(request) :
+    done_todo_id = request.GET['todoNum']
+    print("deleteTodo : 완료한 todo의 id", done_todo_id)
+    todo = Todo.objects.get(id = done_todo_id)
+    todo.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+
+
